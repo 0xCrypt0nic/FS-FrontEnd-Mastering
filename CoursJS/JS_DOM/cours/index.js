0b44f8d3d1c421c7c5920860929dc2c13a35dc40
0xCrypt0nic <n.greneche45@gmail.com>
2024-10-18 22:09:40 +0200

    JS - Mouse Events

--- CoursJS/JS_DOM/cours/index.js
+++ CoursJS/JS_DOM/cours/index.js
@@ -22,3 +22,31 @@ btn2.addEventListener("click", () => {
   response.style.color = "red";
   response.classList.add("show-response");
 });
+
+// MOUSE EVENTS
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+});
+
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0,0,0,0.6)";
+});
+
+questionContainer.addEventListener("mouseout", () => {
+  questionContainer.style.background = "black";
+});
+
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(2deg)";
+});
