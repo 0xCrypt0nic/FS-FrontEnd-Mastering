ec0807d6fbd31bfc3fb5653398d9da40fb06b53b
0xCrypt0nic <n.greneche45@gmail.com>
2024-10-18 22:57:24 +0200

    JS - Scroll Event

--- CoursJS/JS_DOM/cours/index.js
+++ CoursJS/JS_DOM/cours/index.js
@@ -67,3 +67,14 @@ document.addEventListener("keypress", (e) => {
     ring("Enter");
   }
 });
+
+// ========================= SCROLL =========================
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-50px";
+  }
+});
