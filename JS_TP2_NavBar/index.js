8a8b23e348c1db46febb4e2fe199c0c48ecfedff
0xCrypt0nic <n.greneche45@gmail.com>
2024-10-29 06:35:35 +0100

    JS - Navbar project finished

---
+++ JS_TP2_NavBar/index.js
@@ -0,0 +1,14 @@
+// Créer un événément au scroll
+
+// Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
+
+// Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
+
+// Connaitre niveau de scroll (window.scrollY)
+
+let lastScrollPosition = window.scrollY;
+
+document.addEventListener("scroll", () => {
+  navbar.style.top = window.scrollY > lastScrollPosition ? "-60px" : "0px";
+  lastScrollPosition = window.scrollY;
+});
