495886ecfb9bae6665acc6321b91d3387aec3cd5
0xCrypt0nic <n.greneche45@gmail.com>
2024-11-11 07:22:51 +0100

    TP6 - Html / CSS finished

---
+++ JS_TP6_ToDoList/index.js
@@ -0,0 +1,13 @@
+// Créer un formulaire pour taper les todo
+
+// Ajouter sur le dom ce qui est tapé dans l'input à la validatino du form
+// monInput.value
+
+// Supprimer un todolorsque l'on click dessus (.remove())
+
+// Stocker dans le local storage la liste
+
+// Consulter le local storage au lancement de l'application pour rajouter les todos.
+
+const htmlCheckboxEmpty = "&#9744;";
+const htmlCheckboxChecked = "&#9745;";
