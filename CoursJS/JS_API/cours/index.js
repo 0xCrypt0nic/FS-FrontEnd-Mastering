28dd7c98ebe1116aa45dfae78aa95f86b5e5202b
0xCrypt0nic <n.greneche45@gmail.com>
2024-11-14 11:50:33 +0100

    JS - CoursAPI - XMLHttpRequest

---
+++ CoursJS/JS_API/cours/index.js
@@ -0,0 +1,12 @@
+// (Ancienne Méthode) XMLHTTPRequest
+
+function reqListener() {
+  //console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+//req.open("get", "data.txt", true);
+//req.open("get", "data.json", true);
+req.open("get", "https://api.blablagues.net/?rub=blagues", true);
+req.send();
