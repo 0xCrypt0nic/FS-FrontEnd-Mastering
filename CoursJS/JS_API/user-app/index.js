3ca480b4a5880deeed6a20e3ef421211527e46d2
0xCrypt0nic <n.greneche45@gmail.com>
2024-11-17 16:56:36 +0100

    JS - AppUser - App finished

--- CoursJS/JS_API/user-app/index.js
+++ CoursJS/JS_API/user-app/index.js
@@ -1,5 +1,6 @@
 let userData = [];
 
+// Récupère les données de l'API.
 const fetchUser = async () => {
   await fetch("https://randomuser.me/api/?results=24")
     .then((res) => res.json())
@@ -8,6 +9,27 @@ const fetchUser = async () => {
   console.log(userData);
 };
 
+// Formate une date au format JJ mois AA.
+const dateParser = (date) => {
+  let newDate = new Date(date).toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return newDate;
+};
+
+// Calcul le nombre de jours écoulés depuis une date donnée.
+const dayCalc = (date) => {
+  let today = new Date();
+  let todayTimestamp = Date.parse(today);
+  let timestamp = Date.parse(date);
+
+  return Math.ceil((todayTimestamp - timestamp) / 8.64e7);
+};
+
+// Affichage des données.
 const userDisplay = async () => {
   await fetchUser();
 
@@ -17,9 +39,9 @@ const userDisplay = async () => {
         `
         <div class="card">
             <img src=${user.picture.large} alt="photo de ${user.name.last}">
-            <h3>${user.name.first}</h3>
-            <p>${user.location.city}, ${user.dob.date}</p>
-            <em>Membre depuis : ${user.registered.date} jours</em>
+            <h3>${user.name.first} ${user.name.last}</h3>
+            <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
+            <em>Membre depuis : ${dayCalc(user.registered.date)} jours</em>
         </div>
         `
     )
