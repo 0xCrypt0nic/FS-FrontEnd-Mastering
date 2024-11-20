f94287468d3cd829e695b7753581371c3dcf9838
0xCrypt0nic <n.greneche45@gmail.com>
2024-11-20 06:56:37 +0100

    JS - TP9 - Country App started

---
+++ JS_TP9_CountryApp/index.js
@@ -0,0 +1,24 @@
+let listOfCountry = [];
+
+const fetchData = async () => {
+  await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+      listOfCountry = data;
+    });
+
+  console.log(listOfCountry);
+};
+
+fetchData();
+
+// 3 - Passer les données à une variable
+
+// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
+
+// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
+//coutry.name.includes(inputSearch.value);
+
+// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
+
+// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
