5f94be5a90e8be39094d7fba52a9fafa8bddbda8
0xCrypt0nic <n.greneche45@gmail.com>
2024-12-04 07:37:49 +0100

    JS - TypeScript intro

---
+++ CoursJS/JS_Fin/test-js/index.ts
@@ -0,0 +1,11 @@
+let chiffre = 42;
+let chiffre2:number = 42;
+
+chiffre = "coucou";
+chiffre2 = "coucou";
+
+const sayName = (name?:string) => {
+    console.log(`Salut ${name}`);
+}
+
+sayName(23);
