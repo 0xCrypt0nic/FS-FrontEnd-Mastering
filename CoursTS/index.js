f25afa2c738f51119ee95f4c82a912cdbd8b3d23
0xCrypt0nic <n.greneche45@gmail.com>
2024-12-07 08:26:15 +0100

    TS - En cours...

---
+++ CoursTS/index.js
@@ -0,0 +1,6 @@
+console.log("// ====== Tuto TypeScript ====== //");
+// ========== LES VARIABLES ========== //
+var num1 = 24;
+var num2;
+var numOrString;
+num1 = 11;
---
+++ CoursTS/index.ts
@@ -0,0 +1,10 @@
+console.log("// ====== Tuto TypeScript ====== //");
+
+// ========== LES VARIABLES ========== //
+
+let num1 = 24;
+let num2:string;
+let numOrString: number | string;
+
+
+num1 = 11;
