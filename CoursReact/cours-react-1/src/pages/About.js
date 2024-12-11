11e1959b70fbb045f67df058dbf8b537a20be5ce
0xCrypt0nic <n.greneche45@gmail.com>
2024-12-11 08:19:17 +0100

    React - Navbar ok

---
+++ CoursReact/cours-react-1/src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A Propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        saepe voluptatum odit eveniet quos blanditiis nemo voluptas aperiam
+        ipsam ab quam in nesciunt laborum culpa maiores, est error, debitis
+        atque qui! Mollitia non vitae nostrum distinctio maxime? Cupiditate,
+        commodi vero accusamus distinctio quam, dolore, delectus reprehenderit
+        vitae fugiat eligendi odit? Iure aliquam laborum fugiat expedita ratione
+        nostrum, debitis mollitia adipisci consequatur reiciendis modi similique
+        nulla ex numquam eveniet autem iusto placeat exercitationem repudiandae
+        magnam minima. Accusantium, eaque maxime. Suscipit voluptate voluptas
+        fugit est! Quisquam corrupti id, velit voluptatem officiis deleniti
+        consequatur eum itaque, ea consectetur vero, provident molestias vitae
+        aspernatur?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        accusantium molestiae, vero sapiente maiores molestias amet. Nemo vero
+        tempora aliquid laudantium ipsam adipisci! Sed id harum tempore delectus
+        quis perspiciatis sapiente doloribus, expedita eos? Ipsam, eaque vel
+        consectetur magni vitae saepe reprehenderit cupiditate? Laborum
+        reprehenderit ducimus quisquam quo quis assumenda dolores voluptatibus
+        voluptas sit odit incidunt ex consectetur, dolorem architecto, sequi
+        enim qui pariatur eaque dolore maxime recusandae! Accusantium,
+        doloremque quisquam quod culpa doloribus labore voluptas natus
+        distinctio quidem veritatis id. Earum suscipit, vitae dolorum itaque
+        expedita eligendi mollitia, cum nesciunt natus inventore numquam cumque?
+        Quis sequi asperiores dolorem ex. Necessitatibus perferendis quod,
+        dignissimos doloremque voluptas totam a eius fugiat molestias possimus
+        minus placeat sequi magnam, quidem culpa! Quidem maiores, aspernatur
+        eveniet cumque optio voluptate, consequatur dolor sit, reiciendis
+        officia quae autem hic? Quos impedit id ea provident, vel totam,
+        voluptates deleniti ad nisi debitis delectus accusamus laudantium
+        possimus quia, corporis in recusandae voluptatum quidem quibusdam itaque
+        cumque culpa ipsa eaque praesentium? Voluptate ab ratione consectetur
+        amet iusto id dolorem, eveniet facilis rerum dolor corrupti at nihil
+        veritatis numquam iure, dolores fugiat deserunt. Eaque fuga doloremque
+        ad accusamus architecto rem obcaecati optio ipsa. Nulla, omnis cumque.
+        Nulla dolore quibusdam mollitia!
+      </p>
+    </div>
+  );
+};
+
+export default About;
