bb5ed0aa32ebc710afa67d99d4f06397c0725623
0xCrypt0nic <n.greneche45@gmail.com>
2024-12-16 11:38:11 +0100

    React - Radio Select OK

--- CoursReact/cours-react-1/src/components/Countries.js
+++ CoursReact/cours-react-1/src/components/Countries.js
@@ -5,6 +5,7 @@ import Card from "./Card";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState(36);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   // Le useEffect se joue lorsque le composant est monté.
   useEffect(() => {
@@ -23,6 +24,12 @@ const Countries = () => {
           defaultValue={rangeValue}
           onChange={(e) => setRangeValue(e.target.value)}
         />
+        {radios.map((continent) => (
+          <li>
+            <input type="radio" id={continent} name="continentRadio" />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
       </ul>
       <ul>
         {data.slice(0, rangeValue).map((country, index) => (
