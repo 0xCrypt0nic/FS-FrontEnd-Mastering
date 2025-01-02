2d9ff2b3d4c0090c05dd7c68601838960613fe70
0xCrypt0nic <n.greneche45@gmail.com>
2025-01-02 08:20:10 +0100

    TP1 - Header finished

--- React_TP1_CookingApp/tp1/src/components/Header.js
+++ React_TP1_CookingApp/tp1/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1>React App</h1>
+      <h1>Cooking App</h1>
       <input
         type="text"
         placeholder="Tapez le nom d'un aliment (en anglais)"
